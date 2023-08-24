530ed766f864315713926200e0bc2507a5f38d9d
yuanfei <1574133487@qq.com>
2023-08-24 21:00:42 +0800

    feat：string 转int

--- core/utls.py
+++ core/utls.py
@@ -14,7 +14,7 @@ class Pagination(PageNumberPagination):
             'respCode': 200,
             'respMsg': '操作成功!',
             'data': {
-                'page': self.request.query_params.get(self.page_query_param, 1),
+                'page': int(self.request.query_params.get(self.page_query_param, 1)),
                 'pageSize': self.get_page_size(self.request),
                 'total': self.page.paginator.count,  # 重写翻页返回的字段
                 # 'next': self.get_next_link(),
