bc817be0d3b5c8bab1c45dae422b4fbca4307bc0
yuanfei <1574133487@qq.com>
2023-08-25 16:37:43 +0800

    feat：id

--- apps/merchandise/models.py
+++ apps/merchandise/models.py
@@ -20,6 +20,7 @@ class Skus(models.Model):
 
 
 class SpuInfo(models.Model):
+    _id = models.CharField('账号id', max_length=99999)
     name = models.CharField('商品名', max_length=100)
     amount = models.FloatField('商品金额', max_length=99999)
     mainUrl = models.CharField('商品主图', max_length=10000)
