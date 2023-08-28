1e9268de34f63e7c0a5cf9b1014ebd77eff824a2
yuanfei <1574133487@qq.com>
2023-08-28 16:03:32 +0800

    feat：日志文件输出

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -13,8 +13,8 @@ threads = 16  # multiprocessing.cpu_count()*4 #指定每个进程开启的线程
 loglevel = 'info'  # 日志级别，这个日志级别指的是错误日志的级别，而访问日志的级别无法设置
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
 
-# accesslog = "/home/log/gunicorn_access.log"      #访问日志文件
-# errorlog = "/home/log/gunicorn_error.log"        #错误日志文件
+accesslog = "/home/log/gunicorn_access.log"  # 访问日志文件
+errorlog = "/home/log/gunicorn_error.log"  # 错误日志文件
 accesslog = "-"  # 访问日志文件，"-" 表示标准输出
 errorlog = "-"  # 错误日志文件，"-" 表示标准输出
 
